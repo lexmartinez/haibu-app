9b0660c72c90677853498c41b34412e6bae9377e
Lex Martinez <lex0316@gmail.com>
2023-06-28 17:48:32 -0500

    appname update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'transform-flow-strip-types',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+    </SafeAreaView>
+  );
+}
+
+export default App;
