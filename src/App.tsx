80d8b52dccdbcfb81f11975b277dcc4ecda8aa46
Lex Martinez <lex0316@gmail.com>
2023-06-28 18:00:26 -0500

    adding theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,28 @@
 
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-
-import styled from 'styled-components/native';
+import styled, {ThemeProvider} from 'styled-components/native';
+import thm from '~/config/theme';
 
 const StyledView = styled.View`
-  background-color: papayawhip;
+  background-color: ${({theme}: any) => theme.main};
+  padding: 10px 10px;
 `;
 
 const StyledText = styled.Text`
-  color: #bf4f74;
+  color: #fff;
 `;
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={'dark-content'} />
-      <StyledView>
-        <StyledText>Hello World!</StyledText>
-      </StyledView>
-    </SafeAreaView>
+    <ThemeProvider theme={thm}>
+      <SafeAreaView>
+        <StatusBar barStyle={'dark-content'} />
+        <StyledView>
+          <StyledText>Hello World!</StyledText>
+        </StyledView>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
